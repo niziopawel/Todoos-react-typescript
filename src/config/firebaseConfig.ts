ed48178f7fec2bf1c046a5d3d402cada63be7442
niziopawel <niziopawel2@gmail.com>
2020-08-15 20:36:14 +0200

    firebase config

---
+++ src/config/firebaseConfig.ts
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCqqTdnw7XEkDmpKgseeuaMGiZ_AnqeNGA',
+  authDomain: 'todo-58e66.firebaseapp.com',
+  databaseURL: 'https://todo-58e66.firebaseio.com',
+  projectId: 'todo-58e66',
+  storageBucket: 'todo-58e66.appspot.com',
+  messagingSenderId: '993552781100',
+  appId: '1:993552781100:web:a0032b945fe9cff6d56689',
+  measurementId: 'G-QM48FP9ECQ',
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+const db = firebase.firestore()
+const auth = firebase.auth()
+
+export { db, auth }
